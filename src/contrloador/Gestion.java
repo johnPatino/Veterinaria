4ac9b5179a2accdd9c117929c56f6d5d9686e9bd
BernardoChuchuca <47090582+BernardoChuchuca@users.noreply.github.com>
2019-01-28 20:30:50 -0500

    Add files via upload

---
+++ src/contrloador/Gestion.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package contrloador;
+
+/**
+ *
+ * @author Bernardo
+ */
+
+public class Gestion {
+    
+    public String roll;
+    
+    public void Roll(String roll){
+    this.roll=roll;
+        
+        
+    }
+    
+    public String Rol(){
+        
+    return roll;
+    }
+    
+}
