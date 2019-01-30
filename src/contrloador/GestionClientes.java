130f3920b8da105ec219af1f31cac4bf221a4be2
BryanAndres25 <47094533+BryanAndres25@users.noreply.github.com>
2019-01-29 19:44:26 -0500

    Add files via upload

--- src/contrloador/GestionClientes.java
+++ src/contrloador/GestionClientes.java
@@ -22,23 +22,23 @@ import modelo.Tipo_telefono;
  * @author Bernardo
  */
 public class GestionClientes {
-   /* public ArrayList<Cliente> getListUsuario(){
-        Telefono tel=new Telefono();
+    public ArrayList<Cliente> getListCliente(){
+       // Telefono tel=new Telefono();
     ArrayList<Cliente> us=new ArrayList<>();
         try {
             Connection ctx=Conexion.getConnection();
             Statement st=ctx.createStatement();
             ResultSet rs=st.executeQuery("SELECT *"
-                    + " FROM usuarios ");
+                    + " FROM VET_CLIENTES ");
             while(rs.next()){
             Cliente cli=new Cliente();
             cli.setId(rs.getInt("CLI_ID"));
             cli.setCedula(rs.getString("CLI_CEDULA").trim());
             cli.setNombre(rs.getString("CLI_NOMBRE").trim());
             cli.setDireccion(rs.getString("CLI_DIRECCION").trim());
-            cli.setTelefonos(tel.setNumero(rs.getInt("CLI_TELEFONO")));
+            cli.setTelefono(rs.getString("CLI_TELEFONO").trim());
             
-            us.add(usu);
+            us.add(cli);
                
             }
         } catch (SQLException ex) {
@@ -47,7 +47,7 @@ public class GestionClientes {
             System.out.println("Error en Listado");
         }
     return us;
-    }*/
+    }
     
      public void InsertCliente(Cliente cliente) {
          
@@ -61,7 +61,7 @@ public class GestionClientes {
            pst.setString(2,cliente.getCedula());
            pst.setString(3,cliente.getNombre());
            pst.setString(4,cliente.getDireccion());
-           pst.setString(5, cliente.getTelefonos().getNumero());
+           pst.setString(5, cliente.getTelefono());
            pst.setString(6,cliente.getCorreo());
           
           
@@ -74,5 +74,63 @@ public class GestionClientes {
             System.out.println("Error en Ingresar datos");
   }
 }
+    public Cliente getBuscarCliente(String codCli){
+    Cliente cl=null;
+       try {
+            Connection cnx = Conexion.getConnection();
+            Statement st = cnx.createStatement();
+            ResultSet rs = st.executeQuery("SELECT * "
+                    + "   FROM VET_CLIENTES WHERE cli_cedula= '"+codCli+"'");
+            while (rs.next()) {
+             cl=new Cliente();
+             cl.setId(rs.getInt("CLI_ID"));
+             cl.setCedula(rs.getString("CLI_CEDULA"));
+             cl.setNombre(rs.getString("CLI_NOMBRE"));
+             cl.setDireccion(rs.getString("CLI_DIRECCION"));
+             cl.setTelefono(rs.getString("CLI_TELEFONO"));
+             cl.setCorreo(rs.getString("CLI_EMAIL"));
+            
+            }
+        } catch (SQLException  e ) {
+            e.printStackTrace();
+            
+            System.out.println(e.getMessage());
+            
+            System.out.println("Error en buscar");
+        }
+    return cl;
+    }
     
+    public void ActualizarCliente(Cliente cliente){
+        try{
+            Connection cnx= Conexion.getConnection();
+            PreparedStatement pst=cnx.prepareStatement(" UPDATE VET_CLIENTES SET"
+            +"CLI_ID=?,CLI_CEDULA=?,CLI_NOMBRE=?,CLI_DIRECCION=?,CLI_TELEFONO=?,CLI_EMAIL=? WHERE CLI_ID='"+cliente.getId()+"'");
+            pst.setInt(1, cliente.getId());
+            pst.setString(2, cliente.getCedula());
+            pst.setString(3,cliente.getNombre());
+           pst.setString(4, cliente.getDireccion());
+           pst.setString(5, cliente.getTelefono());
+           pst.setString(6, cliente.getCorreo());
+           pst.executeQuery();
+           
+        } catch (SQLException ex){
+            System.out.println(ex.getMessage());
+             System.out.println("Error en Actualizar Dato");
+        }
+    }
+    
+    public void EliminarCliente(String cedula){
+        try{
+            Connection cnx= Conexion.getConnection();
+            PreparedStatement pst = cnx.prepareStatement("DELETE FROM VET_CLIENTES"
+            +"WHERE CLI_CEDULA=?");
+            pst.setString(1, cedula);
+            pst.executeQuery();
+        } catch (SQLException ex){
+            System.out.println(ex.getMessage());
+            System.out.println("Error en Eliminar Datos");
+            
+        }
+    }
 }
